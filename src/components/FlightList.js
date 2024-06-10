94b2849f82299534d2f4108d1a93d19c0d409a21
A1exeYakovlev <118552675+A1exeYakovlev@users.noreply.github.com>
2024-06-10 10:41:19 +0300

    Feat: add sorting for a filtered flight list

--- src/components/FlightList.js
+++ src/components/FlightList.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-export default function FlightList({ filterBy, flightsData, flightsDataLoading, errorLoadingData }) {
+export default function FlightList({ filterBy, sortBy, flightsData, flightsDataLoading, errorLoadingData }) {
 
     const [displayedNumber, setDisplayedNumber] = useState(2);
 
@@ -17,16 +17,31 @@ export default function FlightList({ filterBy, flightsData, flightsDataLoading, 
         }
 
         if (filterBy === "single-transfer") {
-            filteredFlights = flights.filter(flight => flight.flight.legs[0].segments.length === 2 && flight.flight.legs[1].segments.length === 2);
+            filteredFlights = flights.filter(flight => flight?.flight?.legs[0]?.segments.length === 2 && flight?.flight?.legs[1]?.segments.length === 2);
         }
 
         if (filterBy === "no-transfer") {
-            filteredFlights = flights.filter(flight => flight.flight.legs[0].segments.length === 1 && flight.flight.legs[1].segments.length === 1);
+            filteredFlights = flights.filter(flight => flight?.flight?.legs[0]?.segments.length === 1 && flight?.flight?.legs[1]?.segments.length === 1);
         }
 
         console.log(filteredFlights)
         console.log(flightsData)
 
+        if (sortBy === "price-increase") {
+            filteredFlights.sort((flightA, flightB) => flightA?.flight?.price?.total?.amount - flightB?.flight?.price?.total?.amount);
+        }
+
+        if (sortBy === "price-decrease") {
+            filteredFlights.sort((flightA, flightB) => flightB?.flight?.price?.total?.amount - flightA?.flight?.price?.total?.amount);
+        }
+
+        if (sortBy === "travel-duration") {
+            filteredFlights.sort((flightA, flightB) => {
+                const totalDurationA = flightA?.flight?.legs.reduce((total, leg) => total + leg?.duration, 0);
+                const totalDurationB = flightB?.flight?.legs.reduce((total, leg) => total + leg?.duration, 0);
+                return totalDurationA - totalDurationB;
+            })
+        }
     }
 
     return (
@@ -44,19 +59,41 @@ export default function FlightList({ filterBy, flightsData, flightsDataLoading, 
 }
 
 function Flight({ flightsDataLoading, errorLoadingData, flight }) {
+    let singlePassengerTotalPrice;
+    let singlePassengerTotalCur;
+    let carrier;
+    if (flight) {
+        const { carrier: carr, price } = flight.flight;
+        singlePassengerTotalPrice = Number(price?.passengerPrices[0]?.singlePassengerTotal?.amount);
+        singlePassengerTotalCur = price?.passengerPrices[0]?.singlePassengerTotal?.currencyCode;
+        carrier = carr;
+    }
 
 
     return (
         <div className="app__flightlist">
-            {flight && (
-                flight?.flight?.legs.map((leg, i) => (
-                    <FlightLeg flightLeg={leg} flightsDataLoading={flightsDataLoading} errorLoadingData={errorLoadingData} key={leg.segments[0]?.departureDate}></FlightLeg>
-                ))
-            )
-            }
-            <button>Выбрать</button>
-        </div >
-    )
+            <div className="flight-leg">
+                {flightsDataLoading && !errorLoadingData && <p>Данные загружаются</p>}
+                {errorLoadingData && <p>{errorLoadingData}</p>}
+                {flight && !errorLoadingData && (
+                    <div className="flight-leg__top">
+                        <p>{carrier?.caption}</p>
+
+                        <div>
+                            <p>{`${singlePassengerTotalPrice} ${singlePassengerTotalCur}`}</p>
+                            <p>Стоимость для одного возрослого пассажира</p>
+                        </div>
+                    </div>
+                )}
+                {flight && (
+                    flight?.flight?.legs.map((leg, i) => (
+                        <FlightLeg flightLeg={leg} flightsDataLoading={flightsDataLoading} errorLoadingData={errorLoadingData} key={leg.segments[0]?.departureDate}></FlightLeg>
+                    ))
+                )
+                }
+                <button>Выбрать</button>
+            </div>
+        </div>)
 }
 
 function FlightLeg({ flightsDataLoading, errorLoadingData, flightLeg }) {
@@ -71,6 +108,7 @@ function FlightLeg({ flightsDataLoading, errorLoadingData, flightLeg }) {
     let travelDuration = {};
     let transferCount;
 
+
     if (flightLeg) {
 
         const { airline: arln, departureAirport: departAir, departureCity: departCity, departureDate: departDate } = flightLeg?.segments[0];
@@ -84,6 +122,9 @@ function FlightLeg({ flightsDataLoading, errorLoadingData, flightLeg }) {
         travelDuration.hours = Math.floor(duration / 60);
         travelDuration.minutes = duration % 60;
         transferCount = flightLeg?.segments.length - 1;
+
+
+
 
         function getDateInfo(inputData, outputDate) {
             const dateObj = new Date(inputData);
@@ -108,7 +149,7 @@ function FlightLeg({ flightsDataLoading, errorLoadingData, flightLeg }) {
             {flightsDataLoading && !errorLoadingData && <p>Данные загружаются</p>}
             {errorLoadingData && <p>{errorLoadingData}</p>}
             {flightLeg && !errorLoadingData && (
-                <div className="flight-leg">
+                <>
                     <p className="flight-leg__dest">
                         {`${departureCity?.caption}, ${departureAirport?.caption}`}
                         <span>{` (${departureAirport?.uid})`}</span>
@@ -133,7 +174,8 @@ function FlightLeg({ flightsDataLoading, errorLoadingData, flightLeg }) {
                     <p className="flight-leg__airline">
                         {`Рейс выполняет: ${airline.caption}`}
                     </p>
-                </div >)
+
+                </>)
             }
         </>
     )
